9497c08f7bc184df15438b65b91a24b27ee97b64
rabbitism <popmessiah@126.com>
2019-05-06 16:56:58 +0800

    update 1959

---
+++ Solutions/1850/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _1850
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            int sum = 0;
+            int count = 1;
+            for(int i = 1; i< s.Length; i++)
+            {
+                if(s[i]!=s[i-1])
+                {
+                    sum+=count/2;
+                    count=1;
+                }
+                else
+                {
+                    count++;
+                }
+            }
+            sum+=count/2;
+            System.Console.WriteLine(sum);;
+        }
+    }
+}
---
+++ Solutions/1959/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _1959
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = Convert.ToInt32(Console.ReadLine());
+            Queue<int> qA = new Queue<int>();
+            Queue<int> qB = new Queue<int>();
+            string[] a = Console.ReadLine().Split();
+            string[] b = Console.ReadLine().Split();
+            for(int i = 0; i< N; i++)
+            {
+                qA.Enqueue(Convert.ToInt32(a[i]));
+                qB.Enqueue(Convert.ToInt32(b[i]));
+            }
+            while(qA.Count!=0 && qB.Count!=0)
+            {
+                if(qA.Peek()>qB.Peek())
+                {
+                    qB.Dequeue();
+                }
+                else
+                {
+                    qA.Dequeue();
+                }
+            }
+            System.Console.WriteLine(qA.Count+qB.Count);
+
+
+        }
+    }
+}
