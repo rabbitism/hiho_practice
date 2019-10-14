f0883dc120b05cf1c3ec2033f716a1f18632bc71
rabbitism <popmessiah@hotmail.com>
2019-10-14 17:17:34 +0800

    update 1175

---
+++ Solutions/1175/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _1175
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] info = Console.ReadLine().Split();
+            int N = Convert.ToInt32(info[0]);
+            int M = Convert.ToInt32(info[1]);
+            int K = Convert.ToInt32(info[2]);
+
+            int[] counts = new int[N + 1];
+
+            string[] sources = Console.ReadLine().Split();
+            foreach(string source in sources)
+            {
+                counts[Convert.ToInt32(source)] = 1;
+            }
+
+            foreach(int i in counts)
+            {
+                System.Console.WriteLine(i);
+            }
+
+
+
+        }
+    }
+}
